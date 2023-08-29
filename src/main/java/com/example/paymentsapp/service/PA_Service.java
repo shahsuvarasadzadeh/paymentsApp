456e27d5fc8a85fb1983977401c80be2befc9cda
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-08-30 00:01:54 +0400

    @Service

---
+++ src/main/java/com/example/paymentsapp/service/PA_Service.java
@@ -0,0 +1,45 @@
+package com.example.paymentsapp.service;
+
+import com.example.paymentsapp.dto.CreateDto;
+import com.example.paymentsapp.dto.UpdateDTO;
+import com.example.paymentsapp.exceptions.NotFoundException;
+import com.example.paymentsapp.model.StudentModel;
+import com.example.paymentsapp.repository.PA_Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class PA_Service implements PA_Interface{
+    private final PA_Repository paRepository;
+    public PA_Service(PA_Repository paRepository) {
+        this.paRepository = paRepository;
+    }
+    @Override
+    public StudentModel createUser(CreateDto user) {
+        return paRepository.save(new StudentModel(user.getName(),
+                user.getSurname(),user.getEmail(),user.getNumber()));
+    }
+    @Override
+    public Set<StudentModel> getAllUsers() {
+        List<StudentModel> all=paRepository.findAll();
+        return new HashSet<>(all);
+    }
+
+    @Override
+    public StudentModel updateStudent(UpdateDTO updateDTO, Long id) {
+        final StudentModel st=paRepository.findById(id).
+                orElseThrow(() -> new NotFoundException("user not found"));
+        st.setName(updateDTO.getName());
+        st.setSurname(updateDTO.getSurname());
+        st.setEmail(updateDTO.getEmail());
+        st.setNumber(updateDTO.getNumber());
+        return paRepository.save(st);
+    }
+
+    @Override
+    public Set<StudentModel> getUserByNumber(String number) {
+        List<StudentModel> st=paRepository.findByNumber(number);
+        return new HashSet<>(st);
+    }
+}
