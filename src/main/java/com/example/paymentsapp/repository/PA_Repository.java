a00bfee9c4ffaf9a2a0efe147c0dace9e94ffff3
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-08-30 00:02:08 +0400

    @Repository

---
+++ src/main/java/com/example/paymentsapp/repository/PA_Repository.java
@@ -0,0 +1,11 @@
+package com.example.paymentsapp.repository;
+
+import com.example.paymentsapp.model.StudentModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PA_Repository extends JpaRepository<StudentModel,Long> {
+    List<StudentModel> findByNumber(String number);
+}
