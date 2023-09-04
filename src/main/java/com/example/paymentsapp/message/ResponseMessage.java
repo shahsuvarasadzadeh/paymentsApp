5ae37b016a66167b738a7d95f6fdb61141b6de5a
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-09-05 00:19:24 +0400

    Response message

---
+++ src/main/java/com/example/paymentsapp/message/ResponseMessage.java
@@ -0,0 +1,11 @@
+package com.example.paymentsapp.message;
+
+import lombok.Data;
+
+@Data
+public class ResponseMessage {
+    private String message;
+    public ResponseMessage(String message){
+        this.message=message;
+    }
+}
