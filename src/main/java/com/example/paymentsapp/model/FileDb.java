6fdc8cd504a462c9d6de207b448e34656da3cd25
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-09-05 01:15:51 +0400

    data too long for column 'data' at row 1 - helli @Column(name = "data", columnDefinition = "LONGBLOB")

--- src/main/java/com/example/paymentsapp/model/FileDb.java
+++ src/main/java/com/example/paymentsapp/model/FileDb.java
@@ -1,21 +1,44 @@
 package com.example.paymentsapp.model;
 
 import jakarta.persistence.*;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "fileTest")
-@NoArgsConstructor
+@Table(name = "pls")
 public class FileDb {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String type;
+    @Lob
+    @Column(name = "data", columnDefinition = "LONGBLOB") // butun problem bu balacadaymis
     private byte[] data;
+    public FileDb() {
+    }
     public FileDb(String name, String type, byte[] data) {
         this.name = name;
         this.type = type;
+        this.data = data;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public byte[] getData() {
+        return data;
+    }
+    public void setData(byte[] data) {
         this.data = data;
     }
 }
