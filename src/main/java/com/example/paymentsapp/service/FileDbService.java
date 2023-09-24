1a98b1b2da7801fa9b846449361333ad0b9a951d
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-09-24 20:19:55 +0400

    @Repository classes

--- src/main/java/com/example/paymentsapp/service/FileDbService.java
+++ src/main/java/com/example/paymentsapp/service/FileDbService.java
@@ -1,11 +1,8 @@
 package com.example.paymentsapp.service;
-
 import com.example.paymentsapp.model.FileDb;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.List;
-
 public interface FileDbService {
     FileDb store(MultipartFile file) throws IOException;
     FileDb getFileById(Long id);
