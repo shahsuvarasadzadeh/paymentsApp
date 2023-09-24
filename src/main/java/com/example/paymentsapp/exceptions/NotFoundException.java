46f4cbe02654ad994b2db9aed204cda347bf74a7
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-09-24 20:20:59 +0400

    Message class

--- src/main/java/com/example/paymentsapp/exceptions/NotFoundException.java
+++ src/main/java/com/example/paymentsapp/exceptions/NotFoundException.java
@@ -1,10 +1,10 @@
 package com.example.paymentsapp.exceptions;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+import java.io.Serial;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
     public NotFoundException(String message) {
         super(message);
